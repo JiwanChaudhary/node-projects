f69629074abd4548bf969f81284f0a7ba5d6de5d
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-05 23:35:46 +0545

    connection established and code refactored

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,10 +1,10 @@
-require('./db/connect')
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect')
 
 //? Middleware
-app.use(express.json()) 
+app.use(express.json())
 
 //? Route
 app.get('/hello', (req, res) => {
@@ -21,4 +21,13 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening on port ${port}...`))
+const start = async () => {
+    try {
+        await connectDB()
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -3,4 +3,10 @@ mongoose.set('strictQuery', true);
 
 const connectionString = 'mongodb+srv://jeewa_n:qe9STQ1ZhSNjWobd@nodeexpress.umjtdvb.mongodb.net/TaskManager?retryWrites=true&w=majority';
 
-mongoose.connect(connectionString).then(() => console.log('Connected successfully...')).catch((err) => console.log(err))
+const connectDB = (url) => {
+    return mongoose.connect(connectionString)
+}
+
+// mongoose.connect(connectionString).then(() => console.log('Connected successfully...')).catch((err) => console.log(err))
+
+module.exports = connectDB
