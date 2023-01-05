d4b718c557516460c4d2f4c699b2d92378535ecd
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-05 23:45:36 +0545

    dotenv file setup

--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
 
-const connectionString = 'mongodb+srv://jeewa_n:qe9STQ1ZhSNjWobd@nodeexpress.umjtdvb.mongodb.net/TaskManager?retryWrites=true&w=majority';
 
 const connectDB = (url) => {
-    return mongoose.connect(connectionString)
+    return mongoose.connect(url)
 }
 
 // mongoose.connect(connectionString).then(() => console.log('Connected successfully...')).catch((err) => console.log(err))
