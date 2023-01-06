88da4df36553518c0a1e03a49a415d45017c2ef0
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-07 00:58:44 +0545

    getting single task by the method findOne using _id

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -16,7 +16,17 @@ const createTask = async (req, res) => {
         res.status(500).json({ msg: error })
     }
 }
-const getTask = (req, res) => {
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOne({ _id: taskID })
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id : ${taskID}` })
+        }
+        return res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
     res.json({ id: req.params.id })
 }
 const updateTask = (req, res) => {
