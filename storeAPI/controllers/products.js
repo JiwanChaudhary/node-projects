dfa6ef8c083ef647e55eab4ecbe76e10b25815c6
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-09 17:37:45 +0545

    static products read

--- storeAPI/controllers/products.js
+++ storeAPI/controllers/products.js
@@ -1,6 +1,12 @@
+const Product = require('../models/products');
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('Testing Async Errors')
-    res.status(200).json({ msg: 'products testing route' })
+    // throw new Error('Testing Async Errors')
+    const products = await Product.find({
+        // featured: true
+        name: 'entertainment center'
+    })
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
