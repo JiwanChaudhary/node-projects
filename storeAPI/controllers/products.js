819311a1e49a03b2da2da25464fd842cb3465bfe
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-10 23:03:09 +0545

    sorting products static

--- storeAPI/controllers/products.js
+++ storeAPI/controllers/products.js
@@ -2,23 +2,33 @@ const Product = require('../models/products');
 
 const getAllProductsStatic = async (req, res) => {
     // throw new Error('Testing Async Errors')
-    const products = await Product.find({
-        // featured: true
-        // name: 'entertainment center'
-        page: '2'
-    })
+    // const search = 'ab';
+    // const products = await Product.find({})
+    // featured: true
+    // name: { $regex: search, $options: 'i' },
+    // page: '2'
+    // minus(-) diyo vane descending order ma sort hunxa
+    // const products = await Product.find({}).sort('name')
+    // const products = await Product.find({}).sort('-name')
+    // const products = await Product.find({}).sort('price')
+    // const products = await Product.find({}).sort('-price')
+    const products = await Product.find({}).sort('-name price')
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
     // console.log(req.query);
-    const { featured, company } = req.query;
+    const { featured, company, name } = req.query;
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false
     }
     if (company) {
         queryObject.company = company
+    }
+    if (name) {
+        // queryObject.name = name
+        queryObject.name = { $regex: name, $options: 'i' }
     }
     console.log(queryObject);
     const products = await Product.find(queryObject)
