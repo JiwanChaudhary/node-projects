ad8d23cdf90396883548563154db411483364d8c
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-10 17:52:41 +0545

    queryObject Added

--- storeAPI/controllers/products.js
+++ storeAPI/controllers/products.js
@@ -4,14 +4,21 @@ const getAllProductsStatic = async (req, res) => {
     // throw new Error('Testing Async Errors')
     const products = await Product.find({
         // featured: true
-        name: 'entertainment center'
+        // name: 'entertainment center'
+        page: '2'
     })
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
     // console.log(req.query);
-    const products = await Product.find(req.query)
+    const { featured } = req.query;
+    const queryObject = {}
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    console.log(queryObject);
+    const products = await Product.find(queryObject)
     res.status(200).json({ products, nbHits: products.length })
 }
 
