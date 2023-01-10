0149a346b1a2d765a395b56c41831082b2750f9f
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-10 23:17:05 +0545

    products sorted dynamically

--- storeAPI/controllers/products.js
+++ storeAPI/controllers/products.js
@@ -18,7 +18,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     // console.log(req.query);
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort } = req.query;
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false
@@ -30,8 +30,17 @@ const getAllProducts = async (req, res) => {
         // queryObject.name = name
         queryObject.name = { $regex: name, $options: 'i' }
     }
-    console.log(queryObject);
-    const products = await Product.find(queryObject)
+    // console.log(queryObject);
+    // const products = await Product.find(queryObject)
+    result = Product.find(queryObject)
+    if (sort) {
+        // console.log(sort);
+        const sortList = sort.split(',').join('');
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+    const products = await result
     res.status(200).json({ products, nbHits: products.length })
 }
 
