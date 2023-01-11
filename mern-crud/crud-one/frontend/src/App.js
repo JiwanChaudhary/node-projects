e426e8516f745f97df0080fe90072115f87a8cd3
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-11 22:36:01 +0545

    initial setup for the frontend part of mern-crud

---
+++ mern-crud/crud-one/frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CRUD operation in MERN - The frontend part</h1>
+    </div>
+  );
+}
+
+export default App;
