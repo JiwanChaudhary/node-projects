027050e2fdd77fc619b4059b190f07a99ce14326
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-11 22:51:29 +0545

    components added initially and material ui installed

--- mern-crud/crud-one/frontend/src/App.js
+++ mern-crud/crud-one/frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import AddUser from './components/AddUser';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>CRUD operation in MERN - The frontend part</h1>
+      {/* <h1>CRUD operation in MERN - The frontend part</h1> */}
+      <NavBar />
+      <AddUser />
     </div>
   );
 }
---
+++ mern-crud/crud-one/frontend/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { AppBar, Toolbar } from "@mui/material";
+
+const NavBar = () => {
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <p>Material UI initial setup</p>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default NavBar;
