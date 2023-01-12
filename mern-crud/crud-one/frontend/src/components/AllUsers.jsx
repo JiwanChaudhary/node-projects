02a1f60e14119222bb190ae5935e233e1451467a
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-13 00:07:27 +0545

    error while using useState hook

--- mern-crud/crud-one/frontend/src/components/AllUsers.jsx
+++ mern-crud/crud-one/frontend/src/components/AllUsers.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { getUsers } from "../services/api";
 
 const AllUsers = () => {
-  return <div>AllUsers</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getAllUsers();
+  }, [users]);
+
+  const getAllUsers = async () => {
+    let allUsers = await getUsers();
+    console.log(allUsers.data);
+    setUsers(allUsers.data);
+    console.log(setUsers(allUsers.data));
+    // console.log(users.length);
+  };
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>ID</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Username</TableCell>
+          <TableCell>Email</TableCell>
+          <TableCell>Phone</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow>
+            <TableCell>{user._id}</TableCell>
+            <TableCell>{user.name}</TableCell>
+            <TableCell>{user.username}</TableCell>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{user.phone}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default AllUsers;
--- mern-crud/crud-one/frontend/src/services/api.js
+++ mern-crud/crud-one/frontend/src/services/api.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
-const URL = '';
+const URL = 'http://localhost:5000';
 
 export const addUser = async (data) => {
     try {
         return await axios.post(`${URL}/adduser`, data)
     } catch (error) {
         console.log('Error while calling add user api', error);
+    }
+}
+
+export const getUsers = async () => {
+    try {
+        return await axios.get(`${URL}/allusers`)
+    } catch (error) {
+        console.log('Error while using getUsers API', error);
     }
 }
