416fc828c29ce22e0eb183de7284733266a52bc1
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-17 16:11:13 +0545

    mongoose package added and server connected to database

--- mern-crud/login-with-auth/backend/app.js
+++ mern-crud/login-with-auth/backend/app.js
@@ -1,1 +1,20 @@
-console.log('Login-With-Auth');
+require('dotenv').config()
+const express = require('express');
+const connectDB = require('./db/connect');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to database and start server
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
