5a100d7dc64a28a1b39e5924a9aaaf9d9d12272d
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-01-18 22:52:14 +0545

    user editted successfully

--- mern-crud/crud-one/backend/controller/userController.js
+++ mern-crud/crud-one/backend/controller/userController.js
@@ -1,25 +1,58 @@
 const UserDetails = require('../model/userSchema');
 
-const addUser = async (req, res) => {
+const asyncWrapper = (fn) => {
+    return async (req, res, next) => {
+        try {
+            await fn(req, res, next)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+const addUser = asyncWrapper(async (req, res) => {
     const userData = req.body;
     console.log(userData);
     const addUser = new UserDetails(userData)
-    try {
-        await addUser.save()
-        res.status(201).json({ addUser })
-    } catch (error) {
-        res.status(409).json({ message: error.message })
-    }
-}
+    // try {
+    await addUser.save()
+    res.status(201).json({ addUser })
+    // } catch (error) {
+    //     res.status(409).json({ message: error.message })
+    // }
+})
 
-const getUsers = async (req, res) => {
-    try {
-        const allUsers = await UserDetails.find({});
-        // console.log(allUsers);
-        res.status(200).json({ allUsers });
-    } catch (error) {
-        res.status(404).json({ message: error.message })
-    }
-}
+const getUsers = asyncWrapper(async (req, res) => {
+    // try {
+    const allUsers = await UserDetails.find({});
+    // console.log(allUsers);
+    res.status(200).json({ allUsers });
+    // console.log(allUsers);
+    // } catch (error) {
+    //     res.status(404).json({ message: error.message })
+    // }
+})
 
-module.exports = { addUser, getUsers }
+const getUser = asyncWrapper(async (req, res) => {
+    // try {
+    // console.log(req.params.id);
+    const id = req.params.id;
+    // const user = await UserDetails.find({ _id: id });
+    const user = await UserDetails.findById(id);
+    // console.log(allUsers);
+    res.status(200).json({ user });
+    // console.log(allUsers);
+    // } catch (error) {
+    //     res.status(404).json({ message: error.message })
+    // }
+})
+
+const editUser = asyncWrapper(async (req, res) => {
+    const user = req.body;
+    const editUser = new UserDetails(user);
+    const id = req.params.id;
+    await UserDetails.updateOne({ _id: id }, editUser);
+    res.status(201).json(editUser)
+})
+
+module.exports = { addUser, getUsers, getUser, editUser }
--- mern-crud/crud-one/backend/routes/route.js
+++ mern-crud/crud-one/backend/routes/route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router()
-const { addUser, getUsers } = require('../controller/userController');
+const { addUser, getUsers, getUser, editUser } = require('../controller/userController');
 
 router.post('/adduser', addUser)
 router.get('/allusers', getUsers)
+router.get('/:id', getUser)
+router.post('/:id', editUser)
 
 module.exports = router
